05c6b8962f76efdd239fb8f4ac0a053feeee10db
hayesall <alexander@batflyer.net>
2021-07-27 11:36:13 -0400

    ✨ Add `RELEASE_VERSION` variable to artifacts

--- zip_artifacts.sh
+++ zip_artifacts.sh
@@ -15,6 +15,15 @@ echo "Creating build directory"
 rm -rf build
 mkdir build
 
+# Zipfiles contain the name of the dataset and the release version
+#   (e.g. v0.1.0). This should be set prior to running, either with:
+#   `export RELEASE_VERSION=v0.1.0`, or automatically using the tag
+#   in GitHub Actions.
+if [[ -z ${RELEASE_VERSION} ]]; then
+  echo "Error. RELEASE_VERSION not set." 1>&2
+  exit 2
+fi
+
 (
   cd srlearn/
 
@@ -25,7 +34,7 @@ mkdir build
       (
         echo "Zipping $ds"
         cd $ds
-        zip -r ../../build/$ds.zip $ds
+        zip -r ../../build/${ds}_${RELEASE_VERSION}.zip ${ds}
       )
 
     fi
